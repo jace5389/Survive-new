abb7a960f69a43f616e96e19bd8f23119de28f46
jace5389 <allen.jace259@gmail.com>
2025-10-30 12:51:50 -0500

    Initial commit

---
+++ Go.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class AutoMove : MonoBehaviour
+{
+    public float speed = 3f;
+
+    void Update()
+    {
+        // Move to the right every frame
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+}
